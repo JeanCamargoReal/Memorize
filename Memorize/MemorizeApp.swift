0a54ba2334a677f5a828823260f08f9cece314a1
Jean Camargo <jeancamargoreal@icloud.com>
2024-05-22 16:01:57 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
